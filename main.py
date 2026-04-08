0bec476d3118915c76bfb4cf21fa029841ccd94e
Airã Vidal <aira.peres@hotmail.com>
2026-04-08 17:57:30 +0100

    Projeto inicial: Otimizador de Picking Logístico

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿import pandas as pd
+
+def otimizar_rota(arquivo_csv):
+    df = pd.read_csv(arquivo_csv)
+    # Ordena por Corredor e depois por Prateleira
+    rota_otimizada = df.sort_values(by=['corredor', 'prateleira'])
+    return rota_otimizada
+
+if __name__ == "__main__":
+    print('--- Rota de Picking Otimizada ---')
+    print(otimizar_rota('pedidos.csv'))
